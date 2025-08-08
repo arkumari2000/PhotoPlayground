ce06f19c3405061ea89a448c45f372fa6bcee1c4
Archana Kumari <arkumari2000@gmail.com>
2025-08-08 21:19:32 +0530

    Initial Commit

---
+++ PhotoPlayground/PhotoPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPlaygroundApp.swift
+//  PhotoPlayground
+//
+//  Created by Archana Kumari on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoPlaygroundTests/PhotoPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPlaygroundTests.swift
+//  PhotoPlaygroundTests
+//
+//  Created by Archana Kumari on 08/08/25.
+//
+
+import Testing
+@testable import PhotoPlayground
+
+struct PhotoPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
